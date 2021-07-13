2a8598158467049a77527056bb46631467dd0117
Murilo Baptista <muboeira@gmail.com>
2021-07-13 20:54:37 -0300

    calculo recursivo da length de uma lista

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,8 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  def call(list) do
+    _length(list, 0)
   end
+
+  defp _length([], count), do: count
+  defp _length([_head | tail], count), do: _length(tail, count + 1)
 end
